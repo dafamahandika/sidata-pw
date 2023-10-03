3b8483bd3e88783cf2ad55926026e1433aba562b
Abisena <abisena1123@gmail.com>
2023-10-03 14:58:24 +0700

    Update SIDATA

--- routes/Form.js
+++ routes/Form.js
@@ -1,6 +1,7 @@
-import { studentCreate } from "../controllers/Formulir.js";
+import { studentCreate, reaData } from "../controllers/Formulir.js";
 import express from "express";
 
 const routes = express.Router();
 routes.post("/create", studentCreate);
+routes.get("/get", reaData);
 export default routes;
