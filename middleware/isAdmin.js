269f90a62e004457866dacc80a54a793367f6be0
Abisena <abisena1123@gmail.com>
2023-10-15 21:55:19 +0700

    Update Middleware

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -4,18 +4,14 @@ import User from "../models/User.js";
 export const isAdmin = async (req, res, next) => {
   try {
     const refreshToken = req.cookies.refreshToken;
-
     if (!refreshToken) {
       return res.status(401).json({ message: "Anda tidak diizinkan akses." });
     }
-
     const decoded = jwt.verify(refreshToken, "RefreshToken");
     const user = await User.findById(decoded.userId);
-
     if (!user) {
       return res.status(401).json({ message: "Anda tidak diizinkan akses." });
     }
-
     if (user.role === "admin") {
       next();
     } else {
