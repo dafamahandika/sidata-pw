975f853e7edc42d208263c32ea59f08c944bfbe6
Abisena <abisena1123@gmail.com>
2023-10-23 19:54:30 +0700

    Update

--- routes/Form.js
+++ routes/Form.js
@@ -10,15 +10,15 @@ import {
   getOnlyGty,
 } from "../controllers/Formulir.js";
 import { isAdmin } from "../middleware/isAdmin.js";
-import { isMuridGuru } from "../middleware/isMuridGuru.js";
+import { isMurid } from "../middleware/isMurid.js";
 import { isLogin } from "../middleware/isLogin.js";
 import express from "express";
 
 const routes = express.Router();
 routes.use(isLogin);
-routes.post("/rayon", isMuridGuru, isRayon);
-routes.post("/rombel", isMuridGuru, isRombel);
-routes.post("/create", isMuridGuru, studentCreate);
+routes.post("/rayon", isMurid, isRayon);
+routes.post("/rombel", isMurid, isRombel);
+routes.post("/create", isMurid, studentCreate);
 routes.get("/get", isAdmin, reaData);
 routes.put("/update/:id", isAdmin, updateData);
 routes.delete("/delete/:id", isAdmin, delData);
