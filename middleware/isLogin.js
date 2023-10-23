fef7c6de3ff7d95df134e10bbfdb625cd87dca9e
Abisena <abisena1123@gmail.com>
2023-10-23 20:09:02 +0700

    Update

--- middleware/isLogin.js
+++ middleware/isLogin.js
@@ -1,41 +1,27 @@
-// import jwt from "jsonwebtoken";
-// import User from "../models/User.js";
-
-// export const isLogin = async (req, res, next) => {
-//   try {
-//     const refreshToken = req.cookies.refreshToken;
-
-//     if (!refreshToken) {
-//       return res
-//         .status(401)
-//         .json({ message: "Anda harus login atau daftar terlebih dahulu." });
-//     }
-
-//     const decoded = jwt.verify(refreshToken, "RefreshToken");
-//     const user = await User.findById(decoded.userId);
-
-//     if (!user) {
-//       return res
-//         .status(401)
-//         .json({ message: "Anda harus login terlebih dahulu." });
-//     }
-
-//     req.user = user;
-//     next();
-//   } catch (error) {
-//     console.error(error);
-//     res
-//       .status(500)
-//       .json({ message: "Terjadi kesalahan dalam memproses permintaan." });
-//   }
-// };
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
 export const isLogin = async (req, res, next) => {
   try {
-    if (req.isAuthenticated()) {
-      return next();
+    const refreshToken = req.cookies.refreshToken;
+
+    if (!refreshToken) {
+      return res
+        .status(401)
+        .json({ message: "Anda harus login atau daftar terlebih dahulu." });
     }
-    res.status(401).json({ message: "Anda harus login terlebih dahulu." });
+
+    const decoded = jwt.verify(refreshToken, "RefreshToken");
+    const user = await User.findById(decoded.userId);
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "Anda harus login terlebih dahulu." });
+    }
+
+    req.user = user;
+    next();
   } catch (error) {
     console.error(error);
     res
