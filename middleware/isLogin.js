84d704b6a32d5678d37ffd0141b07a7e7b8fa62f
Abisena <abisena1123@gmail.com>
2023-10-23 13:50:59 +0700

    Update again

--- middleware/isLogin.js
+++ middleware/isLogin.js
@@ -1,7 +1,27 @@
-export const isLogin = (req, res, next) => {
-  if (req.session && req.session.user) {
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const isLogin = async (req, res, next) => {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+      return res
+        .status(401)
+        .json({ message: "Anda harus masuk terlebih dahulu." });
+    }
+    const decoded = jwt.verify(refreshToken, "RefreshToken");
+    const user = await User.findById(decoded.userId);
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "Anda harus masuk terlebih dahulu." });
+    }
+    req.user = user;
     next();
-  } else {
-    res.redirect("/login");
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Terjadi kesalahan dalam memproses permintaan." });
   }
 };
