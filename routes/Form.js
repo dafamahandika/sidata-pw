74d9a05b2ed784051c401b755607c8ccf3e0c63e
Abisena <abisena1123@gmail.com>
2023-10-23 19:50:12 +0700

    Update Middleware Again

--- routes/Form.js
+++ routes/Form.js
@@ -10,20 +10,20 @@ import {
   getOnlyGty,
 } from "../controllers/Formulir.js";
 import { isAdmin } from "../middleware/isAdmin.js";
-import { isMurid } from "../middleware/isMurid.js";
+import { isMuridGuru } from "../middleware/isMuridGuru.js";
 import { isLogin } from "../middleware/isLogin.js";
 import express from "express";
 
 const routes = express.Router();
 routes.use(isLogin);
-routes.post("/rayon", isMurid, isRayon);
-routes.post("/rombel", isMurid, isRombel);
-routes.post("/create", isMurid, studentCreate);
+routes.post("/rayon", isMuridGuru, isRayon);
+routes.post("/rombel", isMuridGuru, isRombel);
+routes.post("/create", isMuridGuru, studentCreate);
 routes.get("/get", isAdmin, reaData);
 routes.put("/update/:id", isAdmin, updateData);
 routes.delete("/delete/:id", isAdmin, delData);
 
-routes.get("/rayon", isAdmin, getRayon);  
+routes.get("/rayon", isAdmin, getRayon);
 routes.get("/rombel", isAdmin, getRombel);
 routes.get("/gty", isAdmin, getOnlyGty);
 export default routes;
