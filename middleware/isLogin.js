ebef9027a2810e9d1da8a6be0adcddc1b3b239da
Abisena <abisena1123@gmail.com>
2023-10-24 21:28:58 +0700

    Update isLogin

--- middleware/isLogin.js
+++ middleware/isLogin.js
@@ -3,21 +3,23 @@ import User from "../models/User.js";
 
 export const isLogin = async (req, res, next) => {
   try {
-    const accessToken = req.cookies.accessToken;
+    const refreshToken = req.cookies.refreshToken;
 
-    if (!accessToken) {
+    if (!refreshToken) {
       return res
         .status(401)
-        .json({ message: "Anda harus login terlebih dahulu." });
+        .json({ message: "Anda harus login atau daftar terlebih dahulu." });
     }
 
-    jwt.verify(accessToken, "AccessTokenSecret", async (err, decoded) => {
+    // Verifikasi refreshToken
+    jwt.verify(refreshToken, "RefreshTokenSecret", async (err, decoded) => {
       if (err) {
         return res
           .status(401)
-          .json({ message: "Token akses tidak valid atau kedaluwarsa." });
+          .json({ message: "Refresh token tidak valid atau kedaluwarsa." });
       }
 
+      // Cari pengguna berdasarkan ID yang ada di refreshToken
       const user = await User.findById(decoded.userId);
 
       if (!user) {
