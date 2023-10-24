6f565022bd3c5fa887e31fdf2b3b247245dcd53b
Abisena <abisena1123@gmail.com>
2023-10-24 22:01:40 +0700

    Update

--- middleware/isLogin.js
+++ middleware/isLogin.js
@@ -1,38 +1,25 @@
-import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 export const isLogin = async (req, res, next) => {
   try {
-    const refreshToken = req.cookies.refreshToken;
+    const userId = req.session.userId;
 
-    if (!refreshToken) {
-      return res
-        .status(401)
-        .json({ message: "Anda harus login atau daftar terlebih dahulu." });
+    if (!userId) {
+      const err = new Error("Not authorized! Go back!");
+      err.status = 401;
+      return next(err);
     }
 
-    jwt.verify(
-      refreshToken,
-      process.env.REFRESH_TOKEN_SECRET,
-      async (err, decoded) => {
-        if (err) {
-          return res
-            .status(401)
-            .json({ message: "Refresh token tidak valid atau kedaluwarsa." });
-        }
+    const user = await User.findById(userId);
 
-        const user = await User.findById(decoded.userId);
-
-        if (!user) {
-          return res
-            .status(401)
-            .json({ message: "Anda harus login terlebih dahulu." });
-        }
-
-        req.user = user;
-        next();
-      }
-    );
+    if (user === null) {
+      const err = new Error("Not authorized! Go back!");
+      err.status = 401;
+      return next(err);
+    } else {
+      req.user = user;
+      next();
+    }
   } catch (error) {
     console.error(error);
     res
