eef7135002c2e711c196d64f3dd983d8b2714f70
Abisena <abisena1123@gmail.com>
2023-10-24 22:14:38 +0700

    Update

--- middleware/isLogin.js
+++ middleware/isLogin.js
@@ -1,13 +1,31 @@
-import User from "../models/User";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-export const isLogin = (req, res, next) => {
-  if (req.session.userId) {
-    const result = User.findById(req.session.userId);
-    res.status(200).json(result);
+export const isLogin = async (req, res, next) => {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+
+    if (!refreshToken) {
+      return res
+        .status(401)
+        .json({ message: "Anda harus login atau daftar terlebih dahulu." });
+    }
+
+    const decoded = jwt.verify(refreshToken, "RefreshToken");
+    const user = await User.findById(decoded.userId);
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "Anda harus login terlebih dahulu." });
+    }
+
+    req.user = user;
     next();
-  } else {
-    const err = new Error("Not authorized! Go back!");
-    err.status = 401;
-    return next(err);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Terjadi kesalahan dalam memproses permintaan." });
   }
 };
