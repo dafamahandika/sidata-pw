3590bf7ad8747a1577b33e054a614d1e23265ef4
Abisena <abisena1123@gmail.com>
2023-10-24 22:05:47 +0700

    Update

--- middleware/isLogin.js
+++ middleware/isLogin.js
@@ -2,7 +2,7 @@ import User from "../models/User.js";
 
 export const isLogin = async (req, res, next) => {
   try {
-    const userId = req.session;
+    const userId = req.session.id;
 
     if (!userId) {
       const err = new Error("Not authorized! Go back!");
