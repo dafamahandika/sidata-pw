ffb93b3ee17a47a5f8a516f0e9c43fcfc67bab92
Abisena <abisena1123@gmail.com>
2023-10-28 18:12:25 +0700

    Update Cors

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const app = express();
 app.use(
   cors({
     origin: "*",
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type, Authorization, Accept, X-Requested-With",
+    methods: "*",
+    allowedHeaders: "*",
     credentials: true,
   })
 );
