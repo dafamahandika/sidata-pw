5375af4d3f03137cabc73a6d73dfee4d43cbc38b
Abisena <abisena1123@gmail.com>
2023-10-28 17:00:14 +0700

    Update

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 import express from "express";
 import db from "./config/conn.js";
-import bodyParser from "body-parser";
 import create from "./routes/Form.js";
 import register from "./routes/Regist.js";
 import Login from "./routes/Login.js";
 import refreshToken from "./routes/geToken.js";
 import createStatus from "./routes/gtkRoutes.js";
 import createJenis from "./routes/gtkRoutes.js";
+import cors from "cors";
 import cookieParser from "cookie-parser";
-// import config from "dotenv";
 const port = process.env.PORT || 3000;
 
 const app = express();
 
-// const accessTokenSecret = process.env.TOKEN_KEY;
-// const refreshTokenSecret = process.env.REFRESH_TOKEN;
-
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
@@ -25,21 +22,6 @@ app.use(Login);
 app.use(refreshToken);
 app.use(createStatus);
 app.use(createJenis);
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
 
 db.on("error", (err) => {
   console.log(err);
