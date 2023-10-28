d01b9a6d0135c8601609323029e181fae4a16f7b
Abisena <abisena1123@gmail.com>
2023-10-28 17:06:18 +0700

    Update

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
