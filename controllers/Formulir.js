d13f1ff0ea827d772dc99cbd0017196e8abc269f
Abisena <abisena1123@gmail.com>
2023-11-07 13:24:59 +0700

    Update

--- controllers/Formulir.js
+++ controllers/Formulir.js
@@ -322,23 +322,26 @@ export const studentCreate = async (req, res) => {
       penghasilan_wali,
     } = req.body;
 
+    const isResultDate = req.body;
+    const isDate = new Date(isResultDate);
+
     const newFamily = new Family({
       student_id: savedForm._id,
       nama_ayah: nama_ayah,
       nik_ayah: nik_ayah,
-      tanggal_lahir_ayah: resultDate,
+      tanggal_lahir_ayah: isDate,
       pendidikan_ayah: pendidikan_ayah,
       pekerjaan_ayah: pekerjaan_ayah,
       penghasilan_ayah: penghasilan_ayah,
       nama_ibu: nama_ibu,
       nik_ibu: nik_ibu,
-      tanggal_lahir_ibu: resultDate,
+      tanggal_lahir_ibu: isDate,
       pendidikan_ibu: pendidikan_ibu,
       pekerjaan_ibu: pekerjaan_ibu,
       penghasilan_ibu: penghasilan_ibu,
       nama_wali: nama_wali,
       nik_wali: nik_wali,
-      tanggal_lahir_wali: resultDate,
+      tanggal_lahir_wali: isDate,
       pendidikan_wali: pendidikan_wali,
       pekerjaan_wali: pekerjaan_wali,
       penghasilan_wali: penghasilan_wali,
