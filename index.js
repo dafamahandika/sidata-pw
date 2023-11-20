1a7b550c9ce35c536c6106ea3d0d59a6e9410685
Abisena <abisena1123@gmail.com>
2023-11-20 14:00:27 +0700

    Update

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://172.232.225.139:3001",
+    // origin: "http://172.232.225.139:3001",
+    origin: "https://dashing-logical-redbird.ngrok-free.app",
     methods: "*",
     allowedHeaders: "*",
     exposedHeaders: "*",
