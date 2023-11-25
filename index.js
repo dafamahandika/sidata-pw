4280393c3e3eec46d854eabf815a8d610181d547
Abisena <abisena1123@gmail.com>
2023-11-25 15:19:35 +0700

    Chat

--- index.js
+++ index.js
@@ -9,12 +9,19 @@ import createJenis from "./routes/gtkRoutes.js";
 import passport from "passport";
 import session from "express-session";
 import googleStrategy from "./controllers/authGoogle.js";
-
 import cors from "cors";
+import http from "http";
+import bodyParser from "body-parser";
+import { Server } from "socket.io";
 import cookieParser from "cookie-parser";
+import chatRoutes from "./routes/chat.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 const port = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(
   cors({
@@ -37,6 +44,38 @@ app.use(
   })
 );
 
+app.use(bodyParser.urlencoded({ extended: true }));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use(express.static(__dirname + "/../public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/../public/index.html");
+});
+
+io.on("connection", async (socket) => {
+  console.log("a user connected");
+
+  socket.on("chat message", async (msg) => {
+    const savedMessage = await chatController.saveMessage(msg);
+    io.emit("chat message", savedMessage);
+
+    if (msg.receiver) {
+      io.to(msg.receiver).emit("notification", {
+        message: "New message from admin",
+      });
+    }
+  });
+
+  socket.on("notification_ack", async (receiverId) => {
+    await chatController.markAsRead(receiverId);
+  });
+  socket.emit("load messages", await chatController.getMessages());
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
 app.use("/uploads", express.static("uploads"));
 
 passport.use(googleStrategy);
@@ -46,12 +85,13 @@ app.use(passport.session());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(create);
-app.use(register);
-app.use(Login);
-app.use(refreshToken);
-app.use(createStatus);
-app.use(createJenis);
+app.use("/create", create);
+app.use("/register", register);
+app.use("/login", Login);
+app.use("/refreshToken", refreshToken);
+app.use("/createStatus", createStatus);
+app.use("/createJenis", createJenis);
+app.use("/api", chatRoutes);
 
 db.on("error", (err) => {
   console.log(err);
@@ -61,6 +101,6 @@ db.once("open", () => {
   console.log("Connected to database");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Run Server http://localhost:${port}`);
 });
