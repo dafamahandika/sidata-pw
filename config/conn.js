2d16a71160390dd81cfdbae3ce9bf7fa7995b966
Abisena <abisena1123@gmail.com>
2023-12-04 20:32:38 +0700

    Testing

--- config/conn.js
+++ config/conn.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
 mongoose.connect(
-  "mongodb+srv://bisena:bisena12345@cluster0.cmqrijy.mongodb.net/sidata",
+  // "mongodb+srv://bisena:bisena12345@cluster0.cmqrijy.mongodb.net/sidata",
+  "mongodb://127.0.0.1:27017/sidata",
   {
-    useNewUrlparser: true,
+    useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
